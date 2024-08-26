08a47b76bf27958c70ad8bfef7bf416e1c49909a
Adam Sayer <asayer82@icloud.com>
2024-08-26 11:43:19 +1000

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Adam on 26/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
