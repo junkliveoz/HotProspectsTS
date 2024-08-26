63da9ee3e945e80e8cb5f7390acd3ca75d8ea884
Adam Sayer <asayer82@icloud.com>
2024-08-26 21:13:59 +1000

    Completed Day 80

---
+++ HotProspects/Demos/ContextMenus.swift
@@ -0,0 +1,18 @@
+//
+//  ContextMenus.swift
+//  HotProspects
+//
+//  Created by Adam on 26/8/2024.
+//
+
+import SwiftUI
+
+struct ContextMenus: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContextMenus()
+}
