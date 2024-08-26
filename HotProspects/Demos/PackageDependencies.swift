d56b0a83bc9814baf7323d083df7552a67924ac7
Adam Sayer <asayer82@icloud.com>
2024-08-26 21:49:58 +1000

    Day 81 Complete

---
+++ HotProspects/Demos/PackageDependencies.swift
@@ -0,0 +1,18 @@
+//
+//  PackageDependencies.swift
+//  HotProspects
+//
+//  Created by Adam on 26/8/2024.
+//
+
+import SwiftUI
+
+struct PackageDependencies: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PackageDependencies()
+}
