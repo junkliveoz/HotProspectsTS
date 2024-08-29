628f9f54006f0200687a2194d476eeebaa815af1
Adam Sayer <asayer82@icloud.com>
2024-08-29 15:00:04 +1000

    The twoStraws approach to solving the challenges

---
+++ HotProspects/EditiingView.swift
@@ -0,0 +1,18 @@
+//
+//  EditiingView.swift
+//  HotProspects
+//
+//  Created by Adam on 29/8/2024.
+//
+
+import SwiftUI
+
+struct EditiingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditiingView()
+}
